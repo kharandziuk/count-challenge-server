d7f5191bc6e3e3c0517e2e88c6f52c728ea40bc7
Max Kharandziuk <max.kharandziuk@here.com>
2016-05-27 15:53:55 +0200

    dump express server; generator code moved to prototype file

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-var assert = require('assert');
-var _ = require('lodash');
-var seedrandom = require('seedrandom');
+var express = require('express')
+var app = express()
 
-function getRandomInt(random, min, max) {
-  return Math.floor(random * (max - min + 1)) + min;
-}
+var PORT = 3000
+app.listen(3000, function() {
+  console.log('listen on ', PORT);
+})
 
-function getRandomPhoneNumber(rnd) {
-  var x = getRandomInt(rnd(), 0, Math.pow(10, 10));
-  return _.pad(x, 10, '0');
-}
 
-console.log(getRandomPhoneNumber(seedrandom('seed')));
-var getRandomList = function(seed, count) {
-  var rng = seedrandom(seed);
-  return _.map(_.range(count), _.partial(getRandomPhoneNumber, rng))
-}
-
-assert.deepEqual(
-    getRandomList('seed', 1000000),
-    getRandomList('seed', 1000000)
-)
-console.log(
-    getRandomList('seed', 10)
-)
