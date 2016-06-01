43678c7b01bbaba67550c51885449d93b2e3eb53
Max Kharandziuk <max.kharandziuk@here.com>
2016-06-01 12:09:05 +0200

    finished prototype

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ var getRandomList = require('./prototype')
 var express = require('express')
 var morgan = require('morgan')
 var _ = require('lodash');
+var bodyParser = require('body-parser')
 var app = express()
 
+
 app.use(morgan('combined'))
-app.get('/book', (req, res) => {
+app.use(bodyParser.json())
+app.get('/games', (req, res) => {
   let {size, seed} = req.query
   let list = getRandomList(seed, Number(size))
   res.json({items: list})
+})
+
+app.post('/games', (req, res) => {
+  let {size, seed, answer} = req.body
+
+  let list = getRandomList(seed, Number(size))
+  if(_.uniq(list).length === Number(answer)) {
+    res.json({status: 'Your answer is correct'})
+  } else {
+    res.json({status: 'Your answer is wrong'})
+  }
+
 })
 
 module.exports = app
--- test/server.spec.js
+++ test/server.spec.js
@@ -4,14 +4,28 @@ var S = require('supertest')
 var _ = require('lodash')
 
 
-describe('server', () => {
-  it('smoke', function(done) {
-    let size = 100
-    S(app).get('/book')
+describe('integration', () => {
+  it('get game', function(done) {
+    let size = 3
+    S(app).get('/games')
       .query({seed: 'seed', size})
       .expect((res) => {
         var {body} = res
         expect(body.items.length).to.eql(size)
+      }).end(done)
+  })
+  it('post wrong solution', function(done) {
+    S(app).post('/games')
+      .send({seed: 'seed', size: 3, answer: 2})
+      .expect(({body}) => {
+        expect(body.status).to.equal('Your answer is wrong')
+      }).end(done)
+  })
+  it('post correct solution', function(done) {
+    S(app).post('/games')
+      .send({seed: 'seed', size: 3, answer: 3})
+      .expect(({body}) => {
+        expect(body.status).to.equal('Your answer is correct')
       }).end(done)
   })
 })
